d88a86b3b09681f398734c3b93016a3c6260025d
Arezooshah <167054584+Arezooshah@users.noreply.github.com>
2025-04-28 18:54:39 +0330

    edit  src

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" dir="RTL">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         
       <header className="flex justify-between items-center w-full overflow-hidden">
