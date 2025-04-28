76cc9a493a2412184e7dbcd6be26aa826c8dc49f
Arezooshah <167054584+Arezooshah@users.noreply.github.com>
2025-04-28 18:11:38 +0330

    edit header

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/HomeContainer/HeaderComponent/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +26,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <header className="flex items-center justify-between w-full overflow-hidden">
+          <Header />
+        </header>
         {children}
+        <footer className="flex flex-col gap-12 bg-[#2841e3] mx-auto px-7 py-8 rounded-3xl w-[95%]">
+          {/* <FooterContainer /> */}
+          footer
+        </footer>
+       
       </body>
     </html>
   );
