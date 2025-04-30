7f7346293c807dbd64b5734e6e1f12e658410295
fateme <fateme.ahmadi.asouri.1983@gmail.com>
2025-04-30 14:16:44 +0330

    [u]branch brialant

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,16 +1,13 @@
-import BrilliantHistory from "@/components/HomeContainer/BrilliantHistory/BrilliantHistory";
 
 export default function Home() {
   return (
     <>
     <div className="flex justify-center items-center bg-lime-600 mx-auto h-72 text-4xl">
-      <p>مارو باد باد اوردی خدااااااااااااااااااااااااااااااا
-
-       
+      <p>مارو باد باد اوردی خدااااااااااااااااااااااااااااااا  
       </p>
       
     </div>
-    {/* <BrilliantHistory/> */}
+  
     </>
   );
 }
